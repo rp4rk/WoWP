d43c4ab9d52ec514fd03b8df316ab789c3f28620
Ryan Park <admin@ryanpark.co.uk>
2020-10-24 03:07:58 +0100

    More events!

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ mod parser;
 
 use linereader::LineReader;
 use matcher::loader::{create_hashmap, load_templates, map_to_json};
+use parser::types::LogCell;
 use serde_json::Result;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::fs::File;
 use std::path::Path;
 use std::str::from_utf8;
+
+fn create_json_representation(eventMap: &Value, event: Vec<LogCell>) {}
 
 fn read_lines(map: HashMap<String, Value>) {
     let file = File::open("WoWCombatLog.txt").expect("open");
@@ -17,15 +20,17 @@ fn read_lines(map: HashMap<String, Value>) {
 
     while let Some(line) = reader.next_line() {
         let line = line.expect("Read error");
-        let string = from_utf8(line).expect("parse error");
+        let string = from_utf8(line).expect("Parse error");
         let test = parser::parse_log_line(string);
 
-        // V BROKEN V
-        // println!("{:?}", test.1[0]);
-        // let key = test.1[0]
-        // if map.contains_key(test.1[0]) {
-        //     println!("Match!")
-        // }
+        if let LogCell::Str(v) = test.1[0] {
+            let event_map_result = map.get(v);
+
+            match event_map_result {
+                Some(event_type) => create_json_representation(event_type, test.1),
+                None => println!("Unhandled event {}", v),
+            }
+        }
     }
 }
 
