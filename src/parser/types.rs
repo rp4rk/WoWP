6c79887c685d35a5d3ece5e8f4faf1483b9d10e8
Ryan Park <rp4rk@users.noreply.github.com>
2020-10-27 03:19:27 +0000

    Heal Mapping (#11)
    * Add integer serializer
    
    * Update healing
    
    * update the event template glob

--- src/parser/types.rs
+++ src/parser/types.rs
@@ -1,8 +1,22 @@
-use serde::{self, Serialize};
+use serde::{self, Serialize, Serializer};
+
+// This prevents floats with no remainder from displaying it
+fn integer_serialize<S>(x: &f64, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let has_remainder = x.fract() != 0.0;
+
+    match has_remainder {
+        true => s.serialize_f64(*x),
+        false => s.serialize_i64(*x as i64),
+    }
+}
 
 #[derive(Debug, PartialEq, Serialize)]
 #[serde(untagged)]
 pub enum LogCell<'a> {
+    #[serde(serialize_with = "integer_serialize")]
     Number(f64),
     Str(&'a str),
     Array(Vec<LogCell<'a>>),
