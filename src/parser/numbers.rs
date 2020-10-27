37db6581f88a986e7a54bf3a7340c5c6408f25b5
Ryan Park <admin@ryanpark.co.uk>
2020-10-27 01:12:21 +0000

    Simplify number parsers

--- src/parser/numbers.rs
+++ src/parser/numbers.rs
@@ -1,48 +1,35 @@
 use crate::parser::types::LogCell;
 use nom::{
     bytes::complete::tag,
-    character::complete::{alphanumeric1, digit1},
-    combinator::{map, opt, recognize},
-    sequence::{pair, tuple},
+    character::complete::alphanumeric1,
+    combinator::{map, map_res},
+    number::complete::double,
+    sequence::tuple,
     IResult,
 };
-
-// Parser for fractions, e.g. .34
-fn frac(input: &str) -> IResult<&str, &str> {
-    recognize(pair(tag("."), digit1))(input)
-}
+use std::num::ParseIntError;
 
 // Parser for number cells
 pub fn number_cell(input: &str) -> IResult<&str, LogCell> {
-    let parser = recognize(tuple((opt(tag("-")), digit1, opt(frac))));
-    map(parser, |s| {
-        let n = s.parse().unwrap();
-        LogCell::Number(n)
-    })(input)
+    map(double, LogCell::Number)(input)
+}
+
+fn hex_to_f64<'a>(n: (&str, &'a str)) -> Result<LogCell<'a>, ParseIntError> {
+    let n = i64::from_str_radix(n.1, 16)?;
+
+    Ok(LogCell::Number(n as f64))
 }
 
 // Parses hex numbers such as 0x8
 pub fn hex_cell(input: &str) -> IResult<&str, LogCell> {
     let parser = tuple((tag("0x"), alphanumeric1));
-    map(parser, |s| {
-        let n = i64::from_str_radix(s.1, 16);
-
-        match n {
-            Ok(v) => LogCell::Number(v as f64),
-            Err(e) => panic!(e),
-        }
-    })(input)
+    map_res(parser, hex_to_f64)(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::parser::types::LogCell;
-
-    #[test]
-    fn test_frac() {
-        assert_eq!(frac(".34"), Ok(("", ".34")))
-    }
 
     #[test]
     fn test_number_cell() {
